e3b403b47258eca3c0525cd31fcfbc5bcf412ff6
kmohannarasimha <kodapakamm123@gmail.com>
2023-06-08 16:53:08 +0530

    second commet

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
 import pandas as pd 
+import base64
 
 def recommend(book):
     book_id=books[books['Title']==book]['ids'].values[0]
@@ -21,13 +22,31 @@ def rec_five(books_list):
         first_five.append(b)
     return first_five
 
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('bookbg.jpg')
+
 booksd = pickle.load(open('books_dict.pkl','rb'))
 books = pd.DataFrame(booksd)
 
 
 sim = pickle.load(open('sim.pkl','rb'))
-
+st.image("ineuron logo.jpg",width=150)
 st.title('Book recommender')    
+
+st.sidebar.image("ineuron logo.jpg",width=150)
 
 selected_book=st.selectbox('pls select books',books['Title'].values)
 if st.button('Recommend'):
